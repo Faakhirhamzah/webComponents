b77352ec93302a77385e708ad0eeac4712eeeb8d
Faakhir Hamzah Kalvath Basha <Faakhir.Basha@mrisoftware.com>
2021-01-20 11:48:33 -0500

    Stencil web component

---
+++ web-component-stencil/src/components/side-drawer/side-drawer.tsx
@@ -0,0 +1,26 @@
+import { Component, h, Prop } from "@stencil/core";
+
+@Component({
+    tag: 'side-drawer',
+    styleUrl: './side-drawer.css',
+    shadow: true
+})
+export class SideDrawer {
+  @Prop({ reflect: true }) title: string;
+  @Prop() open: boolean;
+
+  render() {
+    let content = null;
+    if(this.open){
+      content = (
+        <aside>
+            <header><h1>{this.title}</h1></header>
+            <main>
+              <slot />
+            </main>
+          </aside>
+      );
+    }
+    return content;
+  }
+}
